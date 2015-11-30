adf2ce14a606b64d094e7187815628e8edb0622c
Mohammad Abdoli Rad <atkrad@users.noreply.github.com>
2015-11-30 14:29:37 +0330

    Add PHP user

--- assets/install.sh
+++ assets/install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+PHP_USER=php
+
+# Add & config PHP user
+echo "$PHP_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd --create-home --user-group ${PHP_USER}
+
+# Install globally composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
