23634a3e61afe2373d8b92b5c35544984eb301da
Mohammad Abdoli Rad <atkrad@users.noreply.github.com>
2015-12-01 10:44:15 +0330

    Set new "PATH"

--- assets/install.sh
+++ assets/install.sh
@@ -6,7 +6,7 @@ PHP_USER=php
 # Add & config PHP user
 echo "$PHP_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 useradd --create-home -s /bin/bash --user-group ${PHP_USER}
-su --login ${PHP_USER} -c "echo \"export PATH=\$PATH:/home/php/.composer/vendor/bin\" >> /home/${PHP_USER}/.bashrc"
+su --login ${PHP_USER} -c "echo \"export PATH=\$PATH:/home/php/.composer/vendor/bin\" >> /home/${PHP_USER}/.profile"
 
 # Install globally composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
